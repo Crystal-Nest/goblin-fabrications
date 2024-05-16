6874ce69632a7fbe71df5fab2db647d033e8f1d6
Noir <60018820+sZeta99@users.noreply.github.com>
2024-05-16 21:01:06 +0200

    Added range attack

--- fabric/src/main/java/it/crystalnest/goblin_fabrications/entity/custom/GoblinEntity.java
+++ fabric/src/main/java/it/crystalnest/goblin_fabrications/entity/custom/GoblinEntity.java
@@ -7,7 +7,9 @@ import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -15,15 +17,18 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.animal.*;
+import net.minecraft.world.entity.animal.horse.Horse;
+import net.minecraft.world.entity.monster.AbstractSkeleton;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.RangedAttackMob;
 import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -32,12 +37,15 @@ import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.core.object.PlayState;
 
-import java.util.Iterator;
-import java.util.function.Predicate;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
 
-public class GoblinEntity extends Monster implements GeoEntity  {
+
+public class GoblinEntity extends Monster implements GeoEntity, RangedAttackMob{
+
 
     private static final EntityDataAccessor<Boolean> FLEEING = SynchedEntityData.defineId(GoblinEntity.class, EntityDataSerializers.BOOLEAN);
+
 
     private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
 
@@ -57,19 +65,25 @@ public class GoblinEntity extends Monster implements GeoEntity  {
                 .add(Attributes.MAX_HEALTH, 16.0f)
                 .add(Attributes.ATTACK_DAMAGE, 4.0f)
                 .add(Attributes.ATTACK_SPEED, 2.0f)
-                .add(Attributes.MOVEMENT_SPEED, 0.2);
+                .add(Attributes.MOVEMENT_SPEED, 0.2)
+                .add(Attributes.FOLLOW_RANGE, 2.0f);
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(3, new GoblinFleeGoal<Wolf>(this, Wolf.class, 6.0F, 5.0, 2.2));
+        this.goalSelector.addGoal(3, new GoblinFleeGoal<Wolf>(this, Wolf.class, 25.0F, 2.2, 2.2));
+        this.goalSelector.addGoal(5, new RangedBowAttackGoal<>(this, 1.0, 20, 15.0F));
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0));
         this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Turtle.class, 10, true, false, Turtle.BABY_ON_LAND_SELECTOR));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Pig.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Cow.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Fox.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Horse.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Sheep.class, true));
     }
 
     @Override
@@ -86,7 +100,6 @@ public class GoblinEntity extends Monster implements GeoEntity  {
 
     private PlayState predicate(AnimationState<GeoAnimatable> geoAnimatableAnimationState) {
 
-        System.out.println(this.isFleeing());
         if(this.isFleeing()) {
             geoAnimatableAnimationState.getController().setAnimation(RawAnimation.begin().then("flee", Animation.LoopType.LOOP));
         }else{
@@ -104,14 +117,21 @@ public class GoblinEntity extends Monster implements GeoEntity  {
         return cache;
     }
 
+
     @Override
-    protected void populateDefaultEquipmentSlots(RandomSource source, DifficultyInstance difficulty) {
-        super.populateDefaultEquipmentSlots(source, difficulty);
-        this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.BOW));
-    }
-    @Override
-    protected float getStandingEyeHeight(Pose pose, EntityDimensions dimention) {
-        return 0.74F;
+    public void performRangedAttack(LivingEntity $$0, float $$1) {
+        ItemStack $$2 = this.getProjectile(this.getItemInHand(ProjectileUtil.getWeaponHoldingHand(this, Items.BOW)));
+        AbstractArrow $$3 = this.getArrow($$2, $$1);
+        double $$4 = $$0.getX() - this.getX();
+        double $$5 = $$0.getY(0.3333333333333333) - $$3.getY();
+        double $$6 = $$0.getZ() - this.getZ();
+        double $$7 = Math.sqrt($$4 * $$4 + $$6 * $$6);
+        $$3.shoot($$4, $$5 + $$7 * 0.2F, $$6, 1.6F, (float)(14 - this.level().getDifficulty().getId() * 4));
+        this.playSound(SoundEvents.SKELETON_SHOOT, 1.0F, 1.0F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
+        this.level().addFreshEntity($$3);
     }
 
+    protected AbstractArrow getArrow(ItemStack $$0, float $$1) {
+        return ProjectileUtil.getMobArrow(this, $$0, $$1);
+    }
 }
