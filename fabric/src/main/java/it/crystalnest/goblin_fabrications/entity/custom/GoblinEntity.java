29aeb871446d2f5f7459745b6bf28cf93e6ba29d
Noir <60018820+sZeta99@users.noreply.github.com>
2024-08-03 22:28:42 +0200

    WIP flee mecanic

--- fabric/src/main/java/it/crystalnest/goblin_fabrications/entity/custom/GoblinEntity.java
+++ fabric/src/main/java/it/crystalnest/goblin_fabrications/entity/custom/GoblinEntity.java
@@ -1,34 +1,27 @@
 package it.crystalnest.goblin_fabrications.entity.custom;
 
 import it.crystalnest.goblin_fabrications.goals.custom.GoblinFleeGoal;
-import net.minecraft.nbt.CompoundTag;
+
 import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializer;
+
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.Difficulty;
-import net.minecraft.world.DifficultyInstance;
-import net.minecraft.world.entity.*;
+
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+
 import net.minecraft.world.entity.animal.*;
-import net.minecraft.world.entity.animal.horse.Horse;
-import net.minecraft.world.entity.monster.AbstractSkeleton;
+
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.RangedAttackMob;
-import net.minecraft.world.entity.monster.Skeleton;
+
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.entity.projectile.ProjectileUtil;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -37,11 +30,8 @@ import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.core.object.PlayState;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoField;
 
-
-public class GoblinEntity extends Monster implements GeoEntity, RangedAttackMob{
+public class GoblinEntity extends Animal implements GeoEntity{
 
 
     private static final EntityDataAccessor<Boolean> FLEEING = SynchedEntityData.defineId(GoblinEntity.class, EntityDataSerializers.BOOLEAN);
@@ -61,34 +51,36 @@ public class GoblinEntity extends Monster implements GeoEntity, RangedAttackMob{
         this.entityData.set(FLEEING, isFleeing);
     }
     public static AttributeSupplier.Builder setAttributers() {
-        return Monster.createMonsterAttributes()
-                .add(Attributes.MAX_HEALTH, 16.0f)
-                .add(Attributes.ATTACK_DAMAGE, 4.0f)
-                .add(Attributes.ATTACK_SPEED, 2.0f)
-                .add(Attributes.MOVEMENT_SPEED, 0.2)
-                .add(Attributes.FOLLOW_RANGE, 2.0f);
+        return Animal.createLivingAttributes()
+          .add(Attributes.MAX_HEALTH, 16.0f)
+          .add(Attributes.ATTACK_DAMAGE, 4.0f)
+          .add(Attributes.ATTACK_SPEED, 2.0f)
+          .add(Attributes.MOVEMENT_SPEED, 0.3)
+          .add(Attributes.FOLLOW_RANGE, 5.0)
+          .add(Attributes.JUMP_STRENGTH, 2.5f);
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(3, new GoblinFleeGoal<Wolf>(this, Wolf.class, 25.0F, 2.2, 2.2));
-        this.goalSelector.addGoal(5, new RangedBowAttackGoal<>(this, 1.0, 20, 15.0F));
+
+        this.goalSelector.addGoal(1, new FloatGoal(this));
+        this.goalSelector.addGoal(3, new GoblinFleeGoal<Player>(this, Player.class, 50.0F, 1.7, 1.7));
+        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 50.0F));
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0));
-        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
+
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
-        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Pig.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Cow.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Fox.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Horse.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Sheep.class, true));
+
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
         controllers.add(new AnimationController<GeoAnimatable>(this, "controller", this::predicate));
+    }
+
+    @Nullable
+    @Override
+    public AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob ageableMob) {
+        return null;
     }
 
     @Override
@@ -99,7 +91,6 @@ public class GoblinEntity extends Monster implements GeoEntity, RangedAttackMob{
 
 
     private PlayState predicate(AnimationState<GeoAnimatable> geoAnimatableAnimationState) {
-
         if(this.isFleeing()) {
             geoAnimatableAnimationState.getController().setAnimation(RawAnimation.begin().then("flee", Animation.LoopType.LOOP));
         }else{
@@ -118,20 +109,5 @@ public class GoblinEntity extends Monster implements GeoEntity, RangedAttackMob{
     }
 
 
-    @Override
-    public void performRangedAttack(LivingEntity $$0, float $$1) {
-        ItemStack $$2 = this.getProjectile(this.getItemInHand(ProjectileUtil.getWeaponHoldingHand(this, Items.BOW)));
-        AbstractArrow $$3 = this.getArrow($$2, $$1);
-        double $$4 = $$0.getX() - this.getX();
-        double $$5 = $$0.getY(0.3333333333333333) - $$3.getY();
-        double $$6 = $$0.getZ() - this.getZ();
-        double $$7 = Math.sqrt($$4 * $$4 + $$6 * $$6);
-        $$3.shoot($$4, $$5 + $$7 * 0.2F, $$6, 1.6F, (float)(14 - this.level().getDifficulty().getId() * 4));
-        this.playSound(SoundEvents.SKELETON_SHOOT, 1.0F, 1.0F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
-        this.level().addFreshEntity($$3);
-    }
 
-    protected AbstractArrow getArrow(ItemStack $$0, float $$1) {
-        return ProjectileUtil.getMobArrow(this, $$0, $$1);
-    }
 }
