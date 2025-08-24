0d1e3910a95fadb0f95122a4ebd8af01a49affe0
Noir <60018820+sZeta99@users.noreply.github.com>
2025-08-24 12:04:32 +0200

    spawn egg fixed

--- neoforge/src/main/java/it/crystalnest/goblin_fabrications/ModLoader.java
+++ neoforge/src/main/java/it/crystalnest/goblin_fabrications/ModLoader.java
@@ -2,9 +2,13 @@ package it.crystalnest.goblin_fabrications;
 
 import it.crystalnest.goblin_fabrications.entity.EntityRegistry;
 import it.crystalnest.goblin_fabrications.modifier.biome.BiomeModifierRegistry;
+import net.minecraft.world.entity.SpawnPlacementType;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 import org.jetbrains.annotations.ApiStatus;
 
 /**
@@ -22,7 +26,6 @@ public final class ModLoader {
     CommonModLoader.init();
     bus.<EntityAttributeCreationEvent>addListener(event -> EntityRegistry.GOBLIN_EXPLORER.get());
     BiomeModifierRegistry.register(bus);
-
-
   }
+
 }
--- neoforge/src/main/java/it/crystalnest/goblin_fabrications/handler/SpawnEventsHandler.java
+++ neoforge/src/main/java/it/crystalnest/goblin_fabrications/handler/SpawnEventsHandler.java
@@ -1,12 +1,24 @@
 package it.crystalnest.goblin_fabrications.handler;
 
 
+import it.crystalnest.goblin_fabrications.Constants;
+import it.crystalnest.goblin_fabrications.entity.EntityRegistry;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 /**
  * Handles spawn-related events.
  */
+@EventBusSubscriber(modid = Constants.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public final class SpawnEventsHandler {
 
   private SpawnEventsHandler() {}
 
+  @SubscribeEvent
+  private static void handle(RegisterSpawnPlacementsEvent event) {
+    event.register(EntityRegistry.GOBLIN_EXPLORER.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,(entityType, serverLevelAccessor, spawnReason, pos, randomSource)->true, RegisterSpawnPlacementsEvent.Operation.AND );
+  }
 }
